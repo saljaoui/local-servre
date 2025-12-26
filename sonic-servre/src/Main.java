700565f97db2181f5cde3374f7417d0f3f4aa0a1
saljaoui <soufian2022ff@gmail.com>
2025-12-26 10:54:21 +0100

    `Added ConfigLoader class to parse JSON configuration files`

--- sonic-servre/src/Main.java
+++ sonic-servre/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println("mian start soso");
         ServerConfig config = ConfigLoader.load("config/config.json");
+        System.out.println(config);
         // ServerConfig config = ConfigLoader.load("config/config.json");
         // Server server = new Server(config);
         // server.start();
