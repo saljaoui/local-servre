01fe5a70f4fa2d6b8c42bc36bf056a9a4f0cbe27
saljaoui <soufian2022ff@gmail.com>
2025-12-26 22:44:46 +0100

    `Added non-blocking server implementation using NIO`

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import config.model.WebServerConfig;
 import server.Server;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         WebServerConfig config = WebConfigLoader.load();
         config.validate();
