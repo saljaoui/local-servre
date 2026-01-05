1f347dfbb6f39fd4fe192e8349b9f4bbca254407
saljaoui <soufian2022ff@gmail.com>
2026-01-05 23:29:10 +0100

    `Removed import of java.nio.charset.StandardCharsets`

--- src/server/RequestReader.java
+++ src/server/RequestReader.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.StandardCharsets;
 
 import config.model.WebServerConfig.ServerBlock;
 
